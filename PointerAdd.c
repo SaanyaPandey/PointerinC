83f3a7d477dd69940bc7978e2944fe7282d52b60
Saanya <saanya1000@gmail.com>
2025-08-29 14:17:01 +0530

    This shows addition using pointers.

---
+++ PointerAdd.c
@@ -0,0 +1,22 @@
+/*This code will show you the addition of 2 numbers using pointers.*/
+
+#include <stdio.h>
+#include <conio.h>
+int main(){
+  int a , b; //variable declaration
+  //specifying address
+  int *p = &a; 
+  int *q = &b;
+  int result;
+  int *r = &result;
+  //taking user input
+  printf("Enter the first number: ");
+  scanf("%d", &a);
+  printf("Enter the second number you want: ");
+  scanf("%d",&b);
+  //now add them
+  *r = a + b;
+  //show result
+  printf("the result of the addition of two numbers is:%d",*r);
+  return 0;
+}
